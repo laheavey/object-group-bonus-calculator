ddeadc69a236db4097b56e5b6bf80401fffbb878
Lauren <lauren.a.heavey@gmail.com>
2022-11-29 14:55:36 -0600

    add solutions to answers

--- client.js
+++ client.js
@@ -44,14 +44,52 @@ console.log('array of employee data: ',  employees );
 // Ask questions when you don't.
 
 
-
+const employee1 = {
+  name: 'Atticus',
+  employeeNumber: '2405',
+  annualSalary: '47000',
+  reviewRating: 3
+}
 
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
-  
+  let bonusSum = 1;
+  if (employee.reviewRating <= 2 ) {
+    bonusSum;
+  } else if (employee.reviewRating = 3 ) {
+    bonusSum += .04;
+  } else if (employee.reviewRating = 4 ) {
+    bonusSum += .06;
+  } else {
+    bonusSum += .1;
+  }
+  if (employee.employeeNumber.length === 4) {
+    bonusSum += .05;
+  } 
+  if (employee.annualSalary > 65000) {
+    bonusSum -= .01;
+  }
+  bonusSum = Math.min(Math.max(bonusSum, 1), 1.13)
+  let totalComp = Math.floor(employee.annualSalary * bonusSum);
+    let totalBonus = Math.floor(employee.annualSalary * (bonusSum -1));
+    const newObj = {
+      name: employee.name,
+      totalComp,
+      totalBonus,
+      bonusPercentage: Math.floor((bonusSum - 1) * 100)
+    }
+    return newObj;
+  }    
+
+function calculateBonuses () {
+  for (employee of employees){
+    console.log('Calculated values: ', calculateIndividualEmployeeBonus(employee))
+}
+  return;
+}
   
   // return new object with bonus results
 
-}
+//console.log(calculateIndividualEmployeeBonus(employee1));
